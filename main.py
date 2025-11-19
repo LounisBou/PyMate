1ef62abcb23cd18df837b5285467eef556c8cf2e
LounisBou <lounis.bou@gmail.com>
2025-11-19 10:42:13 +0100

    Change package reference from pymate to pydevmate

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from termcolor import colored
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
-def test_pymate_module(module_name):
+def test_pydevmate_module(module_name):
     """
-    Runs the main block of a PyMate module.
+    Runs the main block of a PyDevMate module.
     """
     try:
         logging.info(colored(f"Testing {module_name}...", "yellow"))
-        script_dir = os.path.join(os.path.dirname(__file__), 'pymate', module_name.split('.')[-1] + '.py')
+        script_dir = os.path.join(os.path.dirname(__file__), 'pydevmate', module_name.split('.')[-1] + '.py')
         logging.info('-' * 50 + '\n')
         subprocess.run(['python', script_dir], check=True)
         print()
@@ -26,32 +26,32 @@ def test_pymate_module(module_name):
     except Exception as e:
         raise Exception(f"Error testing {module_name}: {e}")
 
-def discover_pymate_utilities():
+def discover_pydevmate_utilities():
     """
-    Discovers available modules in the `pymate` package.
+    Discovers available modules in the `pydevmate` package.
     Returns a list of module names.
     """
-    package = importlib.import_module("pymate")
+    package = importlib.import_module("pydevmate")
     return [name for _, name, is_pkg in pkgutil.iter_modules(package.__path__) if not is_pkg]
 
 def main():
     """
-    Entry point for running PyMate utilities.
+    Entry point for running PyDevMate utilities.
     """
     
     # Check arguments
-    parser = argparse.ArgumentParser(description='PyMate - A collection of Python utilities.')
+    parser = argparse.ArgumentParser(description='PyDevMate - A collection of Python utilities.')
     parser.add_argument('utility', type=str, nargs='?', help='Name of the utility to test.')
     args = parser.parse_args()
     
     # Log the start of the test
-    logging.info(colored("PyMate - A collection of Python utilities.", "green"))
-    logging.info(colored("Testing PyMate utilities...\n", "yellow"))
+    logging.info(colored("PyDevMate - A collection of Python utilities.", "green"))
+    logging.info(colored("Testing PyDevMate utilities...\n", "yellow"))
 
     # Discover utilities dynamically
-    utilities = discover_pymate_utilities()
+    utilities = discover_pydevmate_utilities()
     if not utilities:
-        logging.warning(colored("No PyMate utilities found.", "red"))
+        logging.warning(colored("No PyDevMate utilities found.", "red"))
         return
     
     # Check if utility argument is provided and exists
@@ -62,7 +62,7 @@ def main():
             return
         utilities = [args.utility]
     else:
-        logging.info(colored(f"Found {len(utilities)} PyMate utilities:", "yellow"))
+        logging.info(colored(f"Found {len(utilities)} PyDevMate utilities:", "yellow"))
         for utility in utilities:
             logging.info(f" - {utility}")
 
@@ -70,16 +70,16 @@ def main():
     nb_errors = 0
     for utility in utilities:
         try:
-            test_pymate_module(f"pymate.{utility}")
+            test_pydevmate_module(f"pydevmate.{utility}")
         except Exception as e:
             print("Error testing utility:", utility)
             nb_errors += 1
     
     logging.info('-' * 50)
     if nb_errors:
-        logging.error(colored(f"PyMate utilities {len(utilities) - nb_errors}/{len(utilities)} passed.", "red"))
+        logging.error(colored(f"PyDevMate utilities {len(utilities) - nb_errors}/{len(utilities)} passed.", "red"))
     else:
-        logging.info(colored("All PyMate utilities passed successfully.", "green"))
+        logging.info(colored("All PyDevMate utilities passed successfully.", "green"))
 
 if __name__ == '__main__':
     main()
